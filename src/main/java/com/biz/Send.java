91ca3d76808fa403032eada9e5da346660483c93
zhang <614796079@qq.com>
2017-04-11 15:16:04 +0800

    add readme

---
+++ src/main/java/com/biz/Send.java
@@ -0,0 +1,31 @@
+package com.biz;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by Administrator on 2017/4/11.
+ */
+public class Send {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        //创建连接
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        //创建一个通道
+        Channel channel = connection.createChannel();
+        //申报一个队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "hello world!";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println("[x] Sent '" + message + "'");
+        channel.close();
+        connection.close();
+    }
+}
